151493212b7c92ea6dfc707196ece54f5373541b
Haider Iqbal <haideri@ebi.ac.uk>
2019-06-10 13:41:42 +0100

    - Remove unnecessary props passing - Add logic for selectedPreplexity

--- src/TSnePlotWidget.js
+++ src/TSnePlotWidget.js
@@ -9,7 +9,10 @@ class TSnePlotWidget extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      metadata: [],
+      plotdata: {
+        metadata: [],
+        perplexities: []
+      },
       selectedColourBy: `inferred_cell_type`,
       selectedColourByCategory: `metadata`,
       metadataErrorMessage: null,
@@ -37,6 +40,12 @@ class TSnePlotWidget extends React.Component {
         [loadingField]: false,
       })
 
+      if (!(this.state.plotdata.metadata.indexOf(`inferred_cell_type`) > -1)) {
+        this.setState({
+          selectedColourBy: this.state.plotdata.metadata[0].value //selects first element of metadata array if inferred_cell_type is not present
+        })
+      }
+
     } catch (e) {
       this.setState({
         [errorMessageField]: `${e.name}: ${e.message}`,
@@ -50,7 +59,7 @@ class TSnePlotWidget extends React.Component {
     const resource = `json/experiments/${experimentAccession}/metadata`
 
     this._fetchAndSetState(
-      resource, atlasUrl, `metadata`, `metadataErrorMessage`, `loadingMetadata`) // this will work once backend code is merged in sc atlas. In meantime, it will not fetch metadata option as there is no endpoint currently in sc atlas
+      resource, atlasUrl, `plotdata`, `metadataErrorMessage`, `loadingMetadata`) // this will work once backend code is merged in sc atlas. In meantime, it will not fetch metadata option as there is no endpoint currently in sc atlas
   }
 
   _onChangeColourBy(colourByCategory, colourByValue) {
@@ -68,43 +77,44 @@ class TSnePlotWidget extends React.Component {
 
     const {height, atlasUrl, suggesterEndpoint} = this.props
     const {wrapperClassName, clusterPlotClassName, expressionPlotClassName} = this.props
-    const {geneId, speciesName, ks,selectedPerplexity, highlightClusters, perplexities, experimentAccession} = this.props
-    const {metadata, selectedColourBy, selectedColourByCategory} = this.state
+    const {geneId, speciesName, ks, experimentAccession} = this.props
+    const {metadata, perplexities} = this.state.plotdata
+    const {selectedColourBy, selectedColourByCategory, loadingMetadata} = this.state
+
+    const perplexitiesOrdered = perplexities.sort((a, b) => a - b)
 
     return (
-      <div>
+      loadingMetadata ?
+        <p className={`row column loading-message`}>Loading, please wait…</p> :
         <div>
-          <TSnePlotView
-            atlasUrl={atlasUrl}
-            suggesterEndpoint={suggesterEndpoint}
-            wrapperClassName={wrapperClassName}
-            clusterPlotClassName={clusterPlotClassName}
-            expressionPlotClassName={expressionPlotClassName}
-            speciesName={speciesName}
-            experimentAccession={experimentAccession}
-            ks={ks} // for future use
-            metadata={metadata}
-            selectedColourBy={selectedColourBy} // used default k value as we are not showing widget controls for now
-            selectedColourByCategory={selectedColourByCategory} // Is the plot coloured by clusters or metadata (as we not showing controls so used 'clusters' as default selected category
-            highlightClusters={highlightClusters} //future use
-            perplexities={perplexities}
-            selectedPerplexity={selectedPerplexity} //default value given for now
-            geneId={geneId}
-            height={height}
-            onSelectGeneId={() => {
-            }} //for future use
-            onChangePerplexity={() => {
-            }} //for future use
-            onChangeColourBy={(colourByCategory, colourByValue) =>
-              this._onChangeColourBy(colourByCategory, colourByValue)
-            }
-            showControls={false} // flag to control weather controls over tsne plots are shown or not
-          />
+          <div>
+            <TSnePlotView
+              atlasUrl={atlasUrl}
+              suggesterEndpoint={suggesterEndpoint}
+              wrapperClassName={wrapperClassName}
+              clusterPlotClassName={clusterPlotClassName}
+              expressionPlotClassName={expressionPlotClassName}
+              speciesName={speciesName}
+              experimentAccession={experimentAccession}
+              ks={ks} // for future use
+              metadata={metadata}
+              selectedColourBy={selectedColourBy} // used default k value as we are not showing widget controls for now
+              selectedColourByCategory={selectedColourByCategory} // Is the plot coloured by clusters or metadata (as we not showing controls so used 'clusters' as default selected category
+              perplexities={perplexities}
+              selectedPerplexity={perplexitiesOrdered[Math.round((perplexitiesOrdered.length - 1) / 2)]} //default value given for now
+              geneId={geneId}
+              height={height}
+              onChangeColourBy={(colourByCategory, colourByValue) =>
+                this._onChangeColourBy(colourByCategory, colourByValue)
+              }
+              showControls={false} // flag to control weather controls over tsne plots are shown or not
+            />
+          </div>
+          <div>
+            To know more about this experiment go to <a target={`_blank`} href={`https://www.ebi.ac.uk/gxa/sc/experiments/` + experimentAccession}> Single
+            Cell Expression Atlas</a>
+          </div>
         </div>
-        <div>
-          To know more about this experiment go to <a target={`_blank`} href={`https://www.ebi.ac.uk/gxa/sc/experiments/` + experimentAccession}> Single Cell Expression Atlas</a>
-        </div>
-      </div>
     )
   }
 }
@@ -116,13 +126,10 @@ TSnePlotWidget.propTypes = {
   expressionPlotClassName: PropTypes.string,
   experimentAccession: PropTypes.string.isRequired,
   geneId: PropTypes.string.isRequired,
-  perplexities: PropTypes.arrayOf(PropTypes.number),
-  selectedPerplexity: PropTypes.number,
   ks: PropTypes.arrayOf(PropTypes.number),
   suggesterEndpoint: PropTypes.string,
   speciesName: PropTypes.string,
-  height: PropTypes.number,
-  highlightClusters: PropTypes.arrayOf(PropTypes.number),
+  height: PropTypes.number
 }
 
 
@@ -134,10 +141,7 @@ TSnePlotWidget.defaultProps = {
   expressionPlotClassName: `small-12 large-6 columns`,
   speciesName: ``,
   height: 800,
-  ks: [],
-  selectedPerplexity: 1,
-  highlightClusters: [],
-  perplexities: []
+  ks: []
 }
 
 export default TSnePlotWidget
